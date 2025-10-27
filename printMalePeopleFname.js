bf68ef92869f8ea9c4fb0efc95c06c4710140810
tekdhami596--lgtm <tekdhami596@gmail.com>
2025-10-27 09:33:04 +0545

    Add program to print male people first name

---
+++ printMalePeopleFname.js
@@ -0,0 +1,8 @@
+const personArray = ["Ram", 'Priya',"Hari"];
+const genderArray = ["male","female","male"];
+
+for (let i=0;i<personArray.length;i++){
+  if(genderArray[i]==="male"){
+    console.log(personArray[i]);
+  }
+}
